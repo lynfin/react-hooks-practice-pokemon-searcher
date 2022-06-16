4d2c66bcf28ff38c63d3af934d44dd4a89559703
Lyn <lynfinman@gmail.com>
2022-06-16 18:07:07 -0400

    controlled search field

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Search() {
+function Search({ searchTerm, onUpdateSearchTerm }) {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input
+          className="prompt"
+          onChange={(e) => onUpdateSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
         <i className="search icon" />
       </div>
     </div>
