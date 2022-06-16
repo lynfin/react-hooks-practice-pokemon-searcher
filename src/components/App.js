69567513609ed8408439918de88ff48deb94d25b
Lyn <lynfinman@gmail.com>
2022-06-16 17:37:41 -0400

    laid out component structure

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import PokemonPage from "./PokemonPage";
 
 function App() {
+  // ┌ └ ─ ├ │
+  // App
+  //  └ PokemonPage
+  //      ├ PokemonForm
+  //      ├ Search
+  //      └ PokemonCollection
+  //          └ PokemonCard
   return (
     <div className="App">
       <PokemonPage />
