314ad5a6daa28d2a686a26de72d1c4931f546a69
Lyn <lynfinman@gmail.com>
2022-06-16 17:45:40 -0400

    created pokemon cards

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+  const { name, hp, sprites } = pokemon;
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={sprites.front} alt="pokemonFront" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
