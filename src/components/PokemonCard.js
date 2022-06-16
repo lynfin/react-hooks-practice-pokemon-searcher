5d21de0984bf265c8c95d3a700ec8ea899d1f141
Lyn <lynfinman@gmail.com>
2022-06-16 17:54:41 -0400

    flipping cards when clicked

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({ pokemon }) {
   const { name, hp, sprites } = pokemon;
+  const [displayedImage, setDisplayedImage] = useState(sprites.front);
+  const handleClick = () => {
+    setDisplayedImage((curImage) => {
+      if (curImage === sprites.front) {
+        return sprites.back;
+      } else {
+        return sprites.front;
+      }
+    });
+  };
   return (
     <Card>
-      <div>
+      <div onClick={handleClick}>
         <div className="image">
-          <img src={sprites.front} alt="pokemonFront" />
+          <img src={displayedImage} alt="pokemonFront" />
         </div>
         <div className="content">
           <div className="header">{name}</div>
